2c741db935ee9543042a8e9f77015515a088c15d
FredrikNilback <fredrik.nilback@gmail.com>
2024-03-27 11:32:40 +0100

    Fredrik branch 1st commit

--- BiluthyrningAB/src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/BiluthyrningAbApplication.java
+++ BiluthyrningAB/src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/BiluthyrningAbApplication.java
@@ -2,12 +2,17 @@ package KNOLN.Inlamningsuppgift2.BiluthyrningAB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import KNOLN.Inlamningsuppgift2.BiluthyrningAB.Objects.*;
 
 @SpringBootApplication
 public class BiluthyrningAbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BiluthyrningAbApplication.class, args);
+		
 	}
-
 }
--- BiluthyrningAB/src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Objects/User.java
+++ BiluthyrningAB/src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Objects/User.java
@@ -7,10 +7,24 @@ import jakarta.persistence.*;
 public class User {
     @Id
     private String Email;
-
     @Column
     private String Password;
-
     @Column
     private String Salt;
+    @Column
+    private String UserName;
+    @Column
+    private String TelephoneNumber;
+    @Column
+    private String Address;
+
+    public User(String Email, String Password, String Salt, String UserName, String TelephoneNumber, String Address) {
+        this.Email = Email;
+        this.Password = Password;
+        this.Salt = Salt;
+        this.UserName = UserName;
+        this.TelephoneNumber = TelephoneNumber;
+        this.Address = Address;
+    }
+
 }
