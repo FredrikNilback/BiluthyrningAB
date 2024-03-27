8a22cd3fa868c35c073d3d2f7a6f13bbce12afe4
Agronomety <109460572+Agronomety@users.noreply.github.com>
2024-03-27 12:23:35 +0100

    sdw

--- BiluthyrningAB/src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Service/CarService.java
+++ BiluthyrningAB/src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Service/CarService.java
@@ -1,4 +1,27 @@
 package KNOLN.Inlamningsuppgift2.BiluthyrningAB.Service;
 
+import KNOLN.Inlamningsuppgift2.BiluthyrningAB.Objects.Car;
+import KNOLN.Inlamningsuppgift2.BiluthyrningAB.Repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CarService {
+    @Autowired
+    private CarRepository repo;
+
+
+
+    public Car addCar(Car car){
+        return repo.save(car);
+    }
+    public void deleteCar(Car car){
+        repo.delete(car);
+    }
+    public Car getCarByLicensePlate(String licensePlate){
+        return repo.findByLicensePlate(licensePlate);
+    }
+
 }
+
+
