2eea7db1a442add3c9bd414829904b7114e2c732
FredrikNilback <fredrik.nilback@gmail.com>
2024-03-29 12:17:22 +0100

    Kryptering + Saltning

--- BiluthyrningAB/src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Cotrollers/UserController.java
+++ BiluthyrningAB/src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Cotrollers/UserController.java
@@ -6,6 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import KNOLN.Inlamningsuppgift2.BiluthyrningAB.Objects.User;
+import java.util.Random;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("users")
@@ -14,11 +18,45 @@ public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("addUser")
-    public ResponseEntity<User> addUser(String email, String password, String salt,
+    public ResponseEntity<User> addUser(String email, String password,
                                         String name, String telephoneNumber, String address) {
-        User user = new User(email, password, salt, name, telephoneNumber, address);
+
+        String salt = saltMaker();
+        String combinedPassword = (password + salt);
+        String hashedPassword = hashPassword(combinedPassword);
+        User user = new User(email, hashedPassword, salt, name, telephoneNumber, address);
         User savedUser = userService.addUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    private String saltMaker(){
+        Random random = new Random();
+        char[] saltChars = new char[5];
+        for (int i = 0; i < 5; ++i) {
+            saltChars[i] = (char)(random.nextInt(126 - 32 + 1) + 32);
+        }
+        String salt = new String(saltChars);
+
+        return salt;
+    }
+
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            md.update(password.getBytes());
+            byte[] hashedBytes = md.digest();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte b : hashedBytes) {
+                stringBuilder.append(String.format("%02x", b));
+            }
+            return stringBuilder.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
