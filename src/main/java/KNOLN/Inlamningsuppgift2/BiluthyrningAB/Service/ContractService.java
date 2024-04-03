3f973812f7ac1ac26cab5caa27c423df2e5bf9d7
osten2600 <kjellberg.olle@outlook.com>
2024-04-03 11:25:41 +0200

    wow

--- src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Service/ContractService.java
+++ src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Service/ContractService.java
@@ -1,9 +1,12 @@
 package KNOLN.Inlamningsuppgift2.BiluthyrningAB.Service;
 
+import KNOLN.Inlamningsuppgift2.BiluthyrningAB.Objects.Car;
 import KNOLN.Inlamningsuppgift2.BiluthyrningAB.Objects.Contract;
 import KNOLN.Inlamningsuppgift2.BiluthyrningAB.Repositories.ContractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ContractService {
@@ -12,5 +15,9 @@ public class ContractService {
 
     public Contract addContract(Contract contract) {return repo.save(contract);}
     public void deleteContract(Contract contract) {repo.delete(contract);}
+    public List<Contract> getContracts(){
+        return repo.findAll();
+    }
+
     public Contract getContractById(int Id) {return repo.findById(Id);}
 }
