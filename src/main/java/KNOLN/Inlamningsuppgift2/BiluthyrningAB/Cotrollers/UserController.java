31d07dc20701c4eb87e26f03ce109bd88ba28be3
FredrikNilback <fredrik.nilback@gmail.com>
2024-04-09 12:32:14 +0200

    getmapping 2

--- src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Cotrollers/UserController.java
+++ src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Cotrollers/UserController.java
@@ -50,7 +50,7 @@ public class UserController {
         }
     }
 
-    @PostMapping("loginUser")
+    @GetMapping("loginUser")
     public ResponseEntity<User> loginUser(@RequestBody ReqUser req) {
         String email = req.email;
         String password = req.password;
