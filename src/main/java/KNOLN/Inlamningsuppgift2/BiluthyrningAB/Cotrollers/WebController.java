e5165cbb5cf1777b8ba3ae140b127b3e43d161c7
FredrikNilback <fredrik.nilback@gmail.com>
2024-04-10 14:53:17 +0200

    WebController

--- src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Cotrollers/WebController.java
+++ src/main/java/KNOLN/Inlamningsuppgift2/BiluthyrningAB/Cotrollers/WebController.java
@@ -7,7 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 
     @GetMapping("")
-    public String goHome(){
+    public String goHome() {
         return "homepage";
+    }
+
+    @GetMapping("Om oss")
+    public String goAbout() {
+        return "about";
+    }
+
+    @GetMapping("Varukorg")
+    public String goCart() {
+        return "cart";
+    }
+
+    @GetMapping("Ny användare")
+    public String goRegister() {
+        return "register";
+    }
+
+    @GetMapping("Profil")
+    public String goProfile() {
+        return "profile";
     }
 }
